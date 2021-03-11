51bfe5d1cb0198ad1d92c172be9c64cc3c211222
Thinking <59522230+Mental-Inferno@users.noreply.github.com>
2021-03-11 18:55:01 -0500

    Update TestingAlgosInC.c

--- TestingAlgosInC.c
+++ TestingAlgosInC.c
@@ -2,12 +2,14 @@
 
 int main (void) {
 
+    
     /*  Quick-Sort Algorithm for a 1D array of int values rom least to greatest
         - Orgainzes values from least at the start of the array to greatest at the end
     
         Function prototype:
         (void)Qsort-1INT-LG(int* arrayPtr, int arraySize)
     */
+
 
    /*  Quick-Sort Algorithm for a 2D array of int values rom least to greatest
         - Orgainzes values in an nxn array from least to greatest
